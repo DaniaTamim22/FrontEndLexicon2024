183f1e081bc67513b068eb9f0099be4ec532715d
Dania Tamim <dania.tamim@live.se>
2024-10-17 10:59:37 +0200

    modify

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,2 +1,10 @@
 const btn = document.querySelector('addBtn')
 const textInput = document.querySelector('text')
+const list = document.querySelector('list')
+
+// btn.addEventListener('click' , (e) => e.{
+//     if(textInput.value.trim() === '')
+//         return
+//     else
+//     list .appendChild
+// })
